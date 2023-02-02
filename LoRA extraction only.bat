8dfb7d2eb3a9299cc9804c47789e6cf02a914281
Kappa <sashawindows@gmail.com>
2023-02-02 19:57:24 +0500

    The main scripts have been updated.
    Now you can extract LoRA models from checkpoints based on SD2.1

--- LoRA extraction only.bat
+++ LoRA extraction only.bat
@@ -1,8 +1,9 @@
 @echo off
 
 echo LAUNCHING LoRA EXTRACTOR
-echo.
+
 :fil1
+echo.
 .\lib\FileToOpen.exe "set fi1=" "%userprofile%\Desktop\*.ckpt;*.safetensors" "Choose a custom model" > %temp_file1%\temp_file1.cmd  
 
 if errorlevel 1 goto :nd
@@ -11,59 +12,70 @@ call %temp_file1%\temp_file1.cmd
 
 echo Custom model: %fi1%
 echo Fi1=%Fi1%
-echo.
-
-
 
 :fil2
+echo.
 .\lib\FileToOpen.exe "set fi2=" "%userprofile%\Desktop\*.ckpt;*.safetensors" "Select the base model" > %temp_file2%\temp_file2.cmd  
 
 if errorlevel 2 goto :nd
 
 call %temp_file2%\temp_file2.cmd
 
-if not exist %fi2% (goto :fil2)
+if not exist %fi2% (goto fil2)
 if %fi2% == %Fi1% (echo The models should be different!
-goto :fil2)
-echo.
+goto fil2)
 
+echo.
 echo Basic model: %fi2%
 echo Fi2=%Fi2%
-echo.
 
+
+:ask_v2
+echo.
+set "v2=Y"
+echo "Have you chosen a checkpoint based on SD2.1? (Y/N): "
+set /p v2=Or press [ENTER] to select [%v2%]: 
 
 :ask_dim
-set "dim=128"
-echo Enter dim (4-8-16-32-64-128-256-360)
+echo.
+set "dim=300"
+echo Enter dim Any number multiple of 8.
+echo for example: 8,16,32,64,128,256,300,512,1024,etc.
 set /p dim=Or press [ENTER] to select [%dim%]:
 
-echo.
-
 :ask_zip
-set "zip=float"
+echo.
+set "zip=fp16"
 echo Enter the compression type (fp16, float)
 set /p zip=Or press [ENTER] to select [%zip%]: 
 
 
 :ask_save
-.\lib\FileToSave.exe "set fis=" ".\*.safetensors" "Choose the path to save and the name of LoRA" "Default_LoRA.safetensors" /overwritePrompt > %temp_save%\temp_save.cmd
-@echo off
+.\lib\FileToSave.exe "set fis=" ".\*.safetensors" "Choose the path to save and the name of LoRA" "" /overwritePrompt > %temp_save%\temp_save.cmd
+
+if errorlevel 1 goto :ask_save
+
 echo.
-
-if errorlevel 1 goto :end
-
 call %temp_save%\temp_save.cmd
 echo The saving path and the name of the LoRA model: %temp_save%
-echo fis=%fis%
+echo fis=%fis%_LoRA%dim%.safetensors
+
 echo.
-
-
-:launch
 echo Extraction:
-python.exe ./lib/extract_LoRA_from_models.py --save_precision %zip% --save_to %fis% --model_org %fi2% --model_tuned %Fi1% --dim %dim%
+
+python.exe ./lib/extract_LoRA_from_models.py %v1_2% --save_precision %zip% --save_to %fis%_LoRA%dim%.safetensors --model_org %fi2% --model_tuned %Fi1% --dim %dim%
+
+if /I "%v2%" == "Y" (
+    python.exe ./lib/extract_LoRA_from_models.py --save_precision %zip% --save_to %fis%_LoRA%dim%.safetensors --model_org %fi2% --model_tuned %Fi1% --dim %dim% --v2
+) else (
+    python.exe ./lib/extract_LoRA_from_models.py --save_precision %zip% --save_to %fis%_LoRA%dim%.safetensors --model_org %fi2% --model_tuned %Fi1% --dim %dim%
+)
+
+
 
 echo.
 echo Extraction completed.
+
 echo.
 pause
 :nd
